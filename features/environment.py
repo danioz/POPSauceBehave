b80e4739f11554cd4784919515e7078b9cf73dda
danioz <79845463+danioz@users.noreply.github.com>
2021-08-15 16:56:34 +0200

    added options to env file

--- features/environment.py
+++ features/environment.py
@@ -6,7 +6,10 @@ from features.config.config import Test_Data
 
 
 def before_scenario(context, scenario):
-    context.driver = webdriver.Chrome(ChromeDriverManager().install())
+    options = webdriver.ChromeOptions()
+    options.headless = True
+    context.driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+    context.driver.delete_all_cookies()
     context.driver.get(Test_Data.BASE_URL)
 
 
